eeacfe913249fcb6c15e4fba4f76abcb80c11dc4
mohammadsahil01 <mohammadsahil01@gmail.com>
2023-10-16 05:17:24 +0530

    created server

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -4,6 +4,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.listen(5000, () => console.log("server running on port   5000"));
+const server_1 = require("@apollo/server");
+const express4_1 = require("@apollo/server/express4");
+async function init() {
+    const app = (0, express_1.default)();
+    app.use(express_1.default.json());
+    //create server
+    const gqlServer = new server_1.ApolloServer({
+        typeDefs: `
+        type Query {
+            name:String
+            say(name:String):String
+        }
+        `,
+        resolvers: {
+            Query: {
+                name: () => `hii`,
+                say: (_, { name }) => `Hey ${name},  How are you?`
+            }
+        }
+    });
+    await gqlServer.start();
+    app.get("/", (req, res) => {
+        res.json({ message: "server is running" });
+    });
+    app.use("/graphql", (0, express4_1.expressMiddleware)(gqlServer));
+    app.listen(5000, () => console.log("server running on port 5000"));
+}
+;
+init();
 //# sourceMappingURL=app.js.map
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,37 @@
 import express from "express";
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 
-const app = express();
+async function init() {
+    const app = express();
+    app.use(express.json())
+    //create server
+    const gqlServer = new ApolloServer({
+        typeDefs:`
+        type Query {
+            name:String
+            say(name:String):String
+        }
+        `,
+        resolvers:{
+            Query:{
+                name:()=>`hii`,
+                say:(_,{name}:{name:string})=>`Hey ${name},  How are you?`
 
-app.listen(5000,()=>console.log("server running on port 5000"))
+            }
+        }
+    });
+
+    await gqlServer.start();
+
+    app.get("/",(req,res)=>{
+        res.json({message:"server is running"})
+    })
+
+    app.use("/graphql",expressMiddleware(gqlServer))
+
+    app.listen(5000,()=>console.log("server running on port 5000"))
+        
+    };
+
+init();
